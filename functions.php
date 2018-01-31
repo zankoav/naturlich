ad8642e89c8878ad6180c1d2c76fad4b276ef2bd
zankoav <zankoav@mail.ru>
2018-01-31 12:07:40 +0300

    clear theme

--- functions.php
+++ functions.php
@@ -11,3 +11,20 @@ require_once 'inc/NaturlithSetup.php';
 
 $naturlith = new NaturlithSetup();
 $naturlith->setup();
+
+add_action('admin_menu', function () {
+    add_menu_page(
+        'Продукты',
+        'Продукты',
+        'manage_options',
+        'site-options',
+        'add_my_setting',
+        'dashicons-cart',
+        4
+    );
+});
+
+function add_my_setting()
+{
+    get_template_part('inc/templates/content', 'admin');
+}
