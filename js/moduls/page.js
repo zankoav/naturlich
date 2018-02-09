b492c228ad8752a59d9fa5445d6aec3f2a5a52ad
zankoav <zankoav@mail.ru>
2018-02-09 12:25:13 +0300

    add contacts page

---
+++ js/moduls/page.js
@@ -0,0 +1,8 @@
+/**
+ * Created by alexandrzanko on 2/9/18.
+ */
+export function page() {
+    jQuery(document).ready(function ($) {
+        $('.sow-form-field-textarea textarea').attr('rows', 8);
+    });
+}
