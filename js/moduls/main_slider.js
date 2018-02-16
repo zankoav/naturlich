ce21f5049ce1c8c86a4a911dbb4e4edf3058fea8
zankoav <zankoav@mail.ru>
2018-02-16 18:47:59 +0300

    update

---
+++ js/moduls/main_slider.js
@@ -0,0 +1,27 @@
+/**
+ * Created by alexandrzanko on 2/16/18.
+ */
+export function slider($) {
+
+    // $('#carouselExample').on('slide.bs.carousel', function (e) {
+    //     var $e = $(e.relatedTarget);
+    //     var idx = $e.index();
+    //     var itemsPerSlide = 4;
+    //
+    //     var totalItems = $('#carouselExample').find(' .carousel-item').length;
+    //
+    //     if (idx >= totalItems - (itemsPerSlide - 1)) {
+    //         var it = itemsPerSlide - (totalItems - idx);
+    //         for (var i = 0; i < it; i++) {
+    //             // append slides to end
+    //             if (e.direction == "left") {
+    //
+    //                 $('#carouselExample').find('.carousel-item').eq(i).appendTo('#carouselExample .carousel-inner');
+    //             }
+    //             else {
+    //                 $('#carouselExample').find('.carousel-item').eq(0).appendTo('#carouselExample .carousel-inner');
+    //             }
+    //         }
+    //     }
+    // });
+}
