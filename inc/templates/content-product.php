cff43a893ceb9ff5e0d8e98803660e590fa4045d
zankoav <zankoav@mail.ru>
2018-02-19 13:01:06 +0300

    inner page complate

--- inc/templates/content-product.php
+++ inc/templates/content-product.php
@@ -1,26 +1,39 @@
 <div class="col-12 col-md-6">
     <?php the_post_thumbnail('large', array(
-        'class' => "img-fluid w-100",
+        'class' => "img-fluid w-100 bg-white p-2",
         'alt' => the_title('', '', 0)
     )); ?>
 </div>
 <div class="col-12 col-md-6 product-description">
-    <h1 class="entry-title text-uppercase mb-0"><?php the_title(); ?></h1>
-    <?php the_excerpt(); ?>
-    <div class="price my-2">
+    <h1 class="entry-title text-uppercase mb-1 mt-3 mt-md-0"><?php the_title(); ?></h1>
+    <p class="category"><?php echo get_the_terms(get_the_ID(), 'naturlith_products_category')[0]->name; ?></p>
+    <p><?php echo get_post_meta(get_the_ID(), 'naturlith_product_description', true); ?></p>
+    <div class="price mt-3">
         <?php
-        $price = get_post_meta(get_the_ID(), 'naturlith_price', true);
-        echo __("Price: "), empty($price) ? __("none") : "<span>$price $</span>";
-        ?>
-    </div>
-    <?php $stock = get_post_meta(get_the_ID(), 'naturlith_stock', true);
-    $stock = $stock == 'on' ? 'Yes' : 'No';
-    ?>
-    <div class="my-2">In stock : <strong><?php echo $stock; ?></strong></div>
-    <div class="my-2">Cement type: <strong><?php echo get_post_meta(get_the_ID(), 'naturlith_cement', true); ?></strong>
+        $showPrice = get_post_meta(get_the_ID(), 'naturlith_product_show_price', true);
+        $price = get_post_meta(get_the_ID(), 'naturlith_product_price', true);
+
+        if ($showPrice) {
+            echo __("Price: "), empty($price) ? __("none") : "<span>$price $</span>";
+        } ?>
     </div>
 </div>
 
-<div class="col-12 entry-content mt-5">
-    <?php the_content(); ?>
+<div class="col-12 entry-content">
+    <ul class="list-group list-group-flush my-3">
+        <?php $allTerms = get_terms('naturlith_products_filter');
+        $postsTerms = wp_get_post_terms(get_the_ID(), 'naturlith_products_filter');
+
+        foreach ($allTerms as $term) : ?>
+            <?php if (in_array($term, $postsTerms)) : ?>
+                <li class="list-group-item bg-transparent"><?php echo $term->name; ?> <i class="fas fa-check text-success"></i>
+                </li>
+            <?php else: ?>
+                <li class="list-group-item bg-transparent"><?php echo $term->name; ?> <i class="fas fa-times"></i></li>
+            <?php endif; ?>
+        <?php endforeach; ?>
+    </ul>
+    <div class="the-content">
+        <?php the_content(); ?>
+    </div>
 </div><!-- .entry-content -->
